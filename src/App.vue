da8557518a05ba7f594ccc4439586284f216ad73
Brayan Duque <bduque@lean-tech.io>
2024-08-12 10:09:52 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+  import GotCard from './components/GotCard.vue';
+</script>
+
+
+<template>
+  <main>
+    <h1>
+      GOT
+    </h1>
+    <Suspense>
+      <template #default>
+        <GotCard />
+      </template>
+      <template #fallback>
+        <div>
+          <p>Loading...</p>
+        </div>
+      </template>
+    </Suspense>
+  </main>
+</template>
+
+
+
+<style scoped>
+</style>
---
+++ src/components/GotCard.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import axios from "axios"
+    const response = await axios.get("https://thronesapi.com/api/v2/Characters")
+    console.log(response)
+</script>
+
+<template>
+    <div>
+        <h1> GOT card view </h1>
+    </div>
+</template>
